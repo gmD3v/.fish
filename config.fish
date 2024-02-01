0bb8df55066bfc9b6319cca085b0799a29fd747c
gmD3v <gussmotab@gmail.com>
2024-01-31 21:17:28 -0400

    load tmux plugins at shell startup

--- config.fish
+++ config.fish
@@ -5,6 +5,7 @@ if status is-interactive
     and not set -q TMUX
     exec tmux attach -t home # Commands to run in interactive sessions can go here
 end
+tmux source-file ~/tmux/.tmux.conf
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
