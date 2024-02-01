231a3cef2c73fa2bd8f0eda92c3ba68352e51d04
gmD3v <gussmotab@gmail.com>
2024-01-31 22:18:07 -0400

    silence stdout

--- config.fish
+++ config.fish
@@ -5,7 +5,7 @@ if status is-interactive
     and not set -q TMUX
     exec tmux attach -t home # Commands to run in interactive sessions can go here
 end
-tmux source-file ~/tmux/.tmux.conf
+tmux source-file ~/tmux/.tmux.conf >/dev/null 2>&1
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
